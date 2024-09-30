58dcd86e2503a34e334a77feb55b8620e7c8b962
barkha-gupta <barkhagupta401@gmail.com>
2024-09-30 17:04:21 +0530

    feat: add header component

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import logo from "@/app/assets/images/logo.png";
+import { FC } from "react";
+
+const Header: FC = () => {
+  return (
+    <div className="bg-header p-6">
+      <div className="bg-white w-24 h-8 flex items-center justify-center rounded-md">
+        <Image src={logo} alt="company logo" priority />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
